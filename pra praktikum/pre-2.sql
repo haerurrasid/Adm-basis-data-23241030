4c85011106866aa23907de52763ca137a40e0e37
haerurrasid <145946530+haerurrasid@users.noreply.github.com>
2025-04-14 10:44:07 +0800

    Add files via upload

---
+++ pra praktikum/pre-2.sql
@@ -0,0 +1,21 @@
+-- nama : Haerur rasid
+-- NIM : 23241030
+-- Kelas : PTI 
+
+-- Melihat Data Dalam mySQL 
+SHOW DATABASES;
+
+-- Memilih Database Yang Digunakan
+USE mysql;
+
+-- Menampilkan Tabel Dalam mySQL
+SHOW TABLES;
+
+-- Mendeskripsikan Tabel user
+DESCRIBE user;
+
+-- Menampilkan isi data dari kolom Host, User, Password
+SELECT Host, User, Password FROM user;
+
+
+
